b7dc2d36ce70629c43c0ba80c015bd15b0480489
unconst <jake@bittensor.com>
2023-10-30 13:50:12 -0500

    fix

--- neurons/validator/foreground.py
+++ neurons/validator/foreground.py
@@ -289,10 +289,10 @@ def log_state( self, forward_event: dict ):
         'n_failures': self.global_state['n_failures'],
         'n_pages': self.global_state['n_pages'],
         'steps_per_second': self.global_state['steps_per_second'],
-        'validation_time': forward_event['validation_time'],
-        'query_time': forward_event['query_time'],
-        'forward_time': forward_event['forward_time'],
-        'step_time': forward_event['step_time'],
+        'validation_time': forward_event['validation_time'] if 'validation_time' in forward_event else 0.0,
+        'query_time': forward_event['query_time'] if 'query_time' in forward_event else 0.0,
+        'forward_time': forward_event['forward_time'] if 'forward_time' in forward_event else 0.0,
+        'step_time': forward_event['step_time'] if 'step_time' in forward_event else 0.0,
     }
 
     # Log using rich.
