60834f004795b59ed3dd7c1cf6fa9a4f41f77d08
unconst <jake@bittensor.com>
2023-10-30 17:28:01 -0500

    fix

--- neurons/validator/run.py
+++ neurons/validator/run.py
@@ -30,7 +30,7 @@ def get_config():
     parser.add_argument( "--alpha", default=0.9, type=float, help="The weight moving average scoring." )
     parser.add_argument( '--learning_rate', default=1e-4, type=float, help='Learning rate for the optimizer.' )
     parser.add_argument( '--max_concurrent_forward', type=int, default=1, help='Number of allowed concurrent foward requests.' )
-    parser.add_argument( '--max_concurrent_forward_per_uid', type=int, default=1, help='Number of allowed concurrent foward requests per uid.')
+    parser.add_argument( '--max_concurrent_forward_per_uid', type=int, default=4, help='Number of allowed concurrent foward requests per uid.')
     parser.add_argument( '--batch_size', type=int, default=3, help='Eval batch size' )
     parser.add_argument( '--sequence_length', type=int, default=512, help='Eval sequence length' )
     parser.add_argument( '--device', type = str, default='cuda' if torch.cuda.is_available() else 'cpu', help='Device to run the miner on.' )
