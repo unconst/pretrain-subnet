4f908d1456594aef464157b73d4e5fdfd97f78e2
EC2 Default User <surcyf@gmail.com>
2023-11-04 21:22:34 +0000

    logging

--- neurons/validator.py
+++ neurons/validator.py
@@ -222,7 +222,7 @@ def run_step( wins_per_epoch, metagraph, wandb_step ):
 
     bt.logging.info(f"average_loss_per_uid = {average_loss_per_uid}")
     bt.logging.info(f"log = {log}")
-    bt.logging.info(f"loss_per_uid_per_batch = {loss_per_uid_per_batch}")
+    bt.logging.info(f"losses_per_uid_per_batch = {loss_per_uid_per_batch}")
 
     # === Compute wins per batch ===
     win_per_step = {}
