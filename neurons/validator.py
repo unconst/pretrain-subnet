2e47c6b0fa85405909c95860ded257bbe2de82c2
unconst <jake@bittensor.com>
2023-11-06 12:12:23 -0600

    write the steps results to correct dir

--- neurons/validator.py
+++ neurons/validator.py
@@ -317,7 +317,7 @@ def run_step( wins_per_epoch, metagraph, wandb_step ):
     for key in list(log): 
         if log[key] == {}: del log[key]
     bt.logging.success(f"Step results: {log}")
-    with open ("step_results.json", "a") as f:
+    with open ( config.full_path + "/step_results.json", "a") as f:
         json.dump(log, f)
     if config.wandb.on: wandb.log( log, step = wandb_step )
 
