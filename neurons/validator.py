4446a5ebbc88563b61360f3b5c2c57bf01ae59eb
unconst <jake@bittensor.com>
2023-11-09 15:20:26 -0600

    add version to validator wanbd

--- neurons/validator.py
+++ neurons/validator.py
@@ -78,6 +78,7 @@ if config.wandb.on:
     config.uid = my_uid
     config.hotkey = wallet.hotkey.ss58_address
     config.run_name = run_name
+    config.version = pretrain.__version__
     config.type = "validator"
     wandb_run =  wandb.init(
         name = run_name,
