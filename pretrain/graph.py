4b67b161a59fe420c21077f4c18f76889220025e
0x2 <75550506+Code0x2@users.noreply.github.com>
2023-12-03 01:41:38 -0800

    fix last_download reset

--- pretrain/graph.py
+++ pretrain/graph.py
@@ -230,6 +230,7 @@ def sync( uid: int, metagraph: typing.Optional[bt.metagraph] = None ) -> bool:
             'last_update': time.time(),
             'uid': uid,
             'hotkey': expected_hotkey,
+            'last_download': heartbeat
         }, f)
 
     # Check if model needs updating
